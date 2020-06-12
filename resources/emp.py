a6e5cbebe41988feb2c68bb651deb5a0227d2e95
AnnanyaV <annanyaved.07@gmail.com>
2020-06-13 02:22:11 +0530

    Final commit

--- resources/emp.py
+++ resources/emp.py
@@ -1,7 +1,10 @@
 from flask_restful import Resource, reqparse
 from db import query
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import create_access_token, jwt_required
 
 class Emp(Resource):
+    @jwt_required
     def get(self):
         parser=reqparse.RequestParser()
         parser.add_argument('empno', type=int, required=True, help='Cannot be blank')
@@ -11,6 +14,8 @@ class Emp(Resource):
         except:
             return {"message": "There was an error connecting to emp table"}, 500
 
+    
+    @jwt_required
     def post(self):
         parser= reqparse.RequestParser()
         parser.add_argument('empno', type=int, required=True, help="empno Cannot be blank")
@@ -58,3 +63,35 @@ class Emp(Resource):
         except:
             return {"message": "There was an error inserting into emp table"}, 500
         return {"message": "Success"}, 201
+
+class User():
+    def __init__(self, empno, ename, password):
+        self.empno=empno
+        self.ename=ename
+        self.password=password
+
+    @classmethod
+    def getUserByEname(cls, ename):
+        result= query(f"""SELECT empno, ename, pass from emp where ename='{ename}' """, return_json=False)
+        if len(result)>0:
+            return User(result[0]['empno'], result[0]['ename'], result[0]['pass'])
+        return None
+
+    @classmethod
+    def getUserByempno(cls, empno):
+        result= query(f"""SELECT empno, ename, pass from emp where empno='{empno}' """, return_json=False)
+        if len(result)>0:
+            return User(result[0]['empno'], result[0]['ename'], result[0]['pass'])
+        return None
+
+class EmpLogin(Resource):
+    def post(self):
+        parser= reqparse.RequestParser()
+        parser.add_argument('ename', type=str, required=True, help="empname Cannot be blank")
+        parser.add_argument('pass', type=str, required=True, help="pass Cannot be blank")
+        data=parser.parse_args()
+        user = User.getUserByEname(data['ename'])
+        if user and safe_str_cmp(user.password, data['pass']):
+            access_token = create_access_token(identity=user.empno, expires_delta=False)
+            return {'access_token': access_token}, 200
+        return {"message":"Invalid Credentials"}, 401
